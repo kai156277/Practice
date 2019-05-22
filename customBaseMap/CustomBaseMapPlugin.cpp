95021554c30511ffb46eab1a09d9c42ea6650955
SI-zhaokai <msidzhaokai@supersurs.com>
2019-05-22 14:18:40 +0800

    feat: 练习使用Qt插件模块

---
+++ customBaseMap/CustomBaseMapPlugin.cpp
@@ -0,0 +1,47 @@
+﻿#include "CustomBaseMapPlugin.h"
+
+#include <QtCore>
+
+#pragma execution_character_set("utf-8")
+
+CustomBaseMapPlugin::CustomBaseMapPlugin()
+{
+    mPos = QPointF(0, 0);
+}
+
+bool CustomBaseMapPlugin::loadFromFile(const QString &file)
+{
+    mPos.setX(QTime::currentTime().minute());
+    mPos.setY(QTime::currentTime().second());
+    return mPixmap.load(file, "PNG");
+}
+
+QString CustomBaseMapPlugin::filter() const
+{
+    return "PNG 图像(*.png )";
+}
+
+QString CustomBaseMapPlugin::suffix() const
+{
+    return "png";
+}
+
+QPixmap CustomBaseMapPlugin::baseMap() const
+{
+    return mPixmap;
+}
+
+QPointF CustomBaseMapPlugin::pos() const
+{
+    return mPos;
+}
+
+QString CustomBaseMapPlugin::name() const
+{
+    return "PNG文件插件";
+}
+
+QString CustomBaseMapPlugin::description() const
+{
+    return "这是用来加载PNG文件的插件";
+}
